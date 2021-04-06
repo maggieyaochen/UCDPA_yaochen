9d62e45936888cff58a45e81c2d3792bb0d97f17
Yao Chen <chenyao89@hotmail.com>
2021-04-06 23:29:30 +0100

    2nd commit

--- Yao Project.py
+++ Yao Project.py
@@ -1,2 +1,9 @@
-print("Hello")
-print("This is Yao's project")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+supermarket_sales= pd.read_csv (r'C:\Users\maggie\Desktop\Python\Project\supermarket_sales - Sheet1.csv')
+print(supermarket_sales)
+print(supermarket_sales.shape)
+print(supermarket_sales.columns)
+print(supermarket_sales.sort_values("Total"))
+print(supermarket_sales.groupby)
