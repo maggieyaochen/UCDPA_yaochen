35c7a5772d2299b50e4e1a50f9b7cf5d0dc456be
Yao Chen <chenyao89@hotmail.com>
2021-04-22 00:21:23 +0100

    project 5

--- Yao Project.py
+++ Yao Project.py
@@ -237,5 +237,41 @@ sns.barplot(data = supermarket_customer_type
             ,ci = None
             )
 plt.show()
-=====================
-=============================
+
+# Iterrow to check customer's satisfaction by Rating
+
+# Rating is higher than 8, find out invoice no.
+for index, row in supermarket_sales.iterrows():
+    print(row['Invoice ID'],row['Rating']<8)
+
+# Rating is lower than 5, find out invoice no.
+for index, row in supermarket_sales.iterrows():
+    print(row['Invoice ID'],row['Rating']<5)
+
+# To view products by customer's rating
+supermarket_rating=supermarket_sales.groupby(['Product line'])['Rating'].count()
+print(supermarket_rating)
+supermarket_rating.columns = ['Rating_all']
+supermarket_rating = supermarket_rating.reset_index()
+print(supermarket_rating)
+
+supermarket_rating_less5=supermarket_sales[supermarket_sales['Rating']<5]
+supermarket_rating_less5_product=supermarket_rating_less5.groupby(['Product line'])['Rating'].count()
+print(supermarket_rating_less5_product)
+supermarket_rating_less5_product.columns = ['Rating_poor']
+supermarket_rating_less5_product = supermarket_rating_less5_product.reset_index()
+print(supermarket_rating_less5_product)
+supermarket_rating_percentage=supermarket_rating_less5_product['Rating']/supermarket_rating['Rating']
+print(supermarket_rating_percentage)
+
+
+###### Customer's rating
+
+supermarket_rating_all = supermarket_sales[['Product line','gross income','Rating']]
+supermarket_rating_all['Rating']=supermarket_rating_all['Rating'].astype(int)
+supermarket_rating_all['Rating']=supermarket_rating_all['Rating'].astype(str)
+print(supermarket_rating_all)
+print(supermarket_rating_all.info())
+
+supermarket_rating_all=supermarket_rating_all(['Product line','Rating'])['gross income'].agg(['sum','mean'])
+print(supermarket_rating_all)
